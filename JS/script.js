ad710dd5ffb124dd9f0980636851e85be0dcdaea
hoàng thanh tùng <hoangthanhtung381999@gmail.com>
2021-09-24 01:17:59 +0700

    trang web ban dong ho

---
+++ JS/script.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots: false,
+    responsive:{
+        0:{
+            items:2
+        },
+        767:{
+            items:3
+        },
+        992:{
+            item:3
+        },
+        1200:{
+            items:4
+        }
+    }
+})
